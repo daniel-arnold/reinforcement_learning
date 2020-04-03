b310dbd40380998cae337d0d641adcf61547d9e2
Dan Arnold <dbarnold@lbl.gov>
2020-04-02 21:39:58 -0700

    added tensorboard
    added tensorboard functionality, added 5 trial runs

--- cartpole/cartpole.py
+++ cartpole/cartpole.py
@@ -9,16 +9,20 @@ import random
 import gym
 import numpy as np
 from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
 import matplotlib.pyplot as plt
+import datetime
+
+#tensorflow inputs
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 
 env_name = "CartPole-v1"
 
 class DQNSolver:
     
-    def __init__(self, observation_space, action_space, epsilon_decay):
+    def __init__(self, observation_space, action_space, epsilon_decay, log_dir):
         
         self.epsilon = 1.0
         self.epsilon_decay = epsilon_decay
@@ -26,14 +30,17 @@ class DQNSolver:
         self.learning_rate = 0.001
         self.gamma = 0.95
         self.MEMORY_SIZE = 2000
-        self.BATCH_SIZE = 64
+        self.BATCH_SIZE = 200
         
         self.action_space = action_space
         #deque: list-like container with fast appends and pops on either end
         self.memory = deque(maxlen=self.MEMORY_SIZE)
         
+        #writer for tensorboard
+        self.summary_writer = tf.summary.FileWriter(log_dir)
+        
         #NN parameters
-        #1 hidden layer NN
+        #1 hidden layer NN      
         self.model = Sequential()
         self.model.add(Dense(24, input_shape=(observation_space,), activation="relu"))
         self.model.add(Dense(24, activation="relu"))
@@ -57,8 +64,8 @@ class DQNSolver:
         #choose best action and return
         return np.argmax(q_vals[0])
     
-    def act_test(self, state):
-        #act method for testing
+    def act_greedy(self, state):
+        #act method for testing - greedy policy
         #predict q_values from present state
         q_vals = self.model.predict(state)
         #choose best action and return
@@ -91,19 +98,30 @@ class DQNSolver:
     def get_epsilon(self):
         return self.epsilon
     
+    def log_results(self, name, val, idx):
+        #log results for visualization in Tensorboard
+        summary = tf.summary.Summary()
+        summary_value = summary.value.add()
+        summary_value.simple_value = val
+        summary_value.tag = name
+        self.summary_writer.add_summary(summary, idx)
+        self.summary_writer.flush()
+            
+###############################################################################
+    
 def plot_results(results, mean_results, episode, num_ticks_to_win):
     plt.clf()
     plt.plot(results,'b', label = "score")
     plt.plot(mean_results,'r',label="mean score")
     tick_plot = np.arange(0,episode)
     plt.plot(tick_plot, np.ones(np.shape(tick_plot)) * num_ticks_to_win,'k-', label="win threshold")
-    plt.show()
     plt.xlabel('Episode Number')
     plt.ylabel('Episode Score')
     plt.title('Cartpole scores')
     plt.legend()
+    plt.show()
 
-def cartpole(num_episodes, epsilon_decay, verbose = True, make_plots = True):
+def cartpole(num_episodes, epsilon_decay, log_dir, verbose = True, make_plots = True):
     
     if verbose:
         print("Solving CartPole with DQN using Keras and TensorFlow")
@@ -115,16 +133,16 @@ def cartpole(num_episodes, epsilon_decay, verbose = True, make_plots = True):
     
     num_ticks_to_win = 195
     
-    #training environment
+    #training environment - exploration policy
     env = gym.make(env_name)
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
     
-    #testing environment
-    env_test = gym.make(env_name)
+    #testing environment - greedy policy
+    env_greedy = gym.make(env_name)
     
     #initalize the DQN solver
-    dqn_solver = DQNSolver(observation_space, action_space, epsilon_decay)
+    dqn_solver = DQNSolver(observation_space, action_space, epsilon_decay, log_dir)
     
     #run forever until we have a DQN that generates a policy that achieves the
     #objectives of the environment
@@ -146,25 +164,28 @@ def cartpole(num_episodes, epsilon_decay, verbose = True, make_plots = True):
             dqn_solver.remember(state, action, reward, next_state, done)
             state = next_state
             
-        #testing
-        state_test = env_test.reset()
-        state_test = np.reshape(state_test, [1, observation_space])
-        done_test = False
+        #test environment - greedy policy
+        state_greedy = env_greedy.reset()
+        state_greedy = np.reshape(state_greedy, [1, observation_space])
+        done_greedy = False
         step = 0
-        while not done_test:
+        while not done_greedy:
             step += 1
             #env.render()
-            action = dqn_solver.act_test(state_test)
-            next_state_test, reward_test, done_test, info_test = env_test.step(action)
+            action = dqn_solver.act_greedy(state_greedy)
+            next_state_greedy, reward_greedy, done_greedy, info_greedy = env_greedy.step(action)
             #every frame robot is balanced, earn +1, else earn -1
             #reward = reward if not done else -reward
-            next_state_test = np.reshape(next_state_test, [1, observation_space])
+            next_state_greedy = np.reshape(next_state_greedy, [1, observation_space])
             #dqn_solver.remember(state, action, reward, next_state, done)
-            state_test = next_state_test
+            state_greedy = next_state_greedy
         
         #number of steps the pole was upright
         scores.append(step)
+        dqn_solver.log_results('scores', step, episode)
+        
         mean_score = np.mean(scores)
+        dqn_solver.log_results('mean_score(100)', mean_score, episode)
         
         score_list.append(step)
         mean_score_list.append(mean_score)
@@ -176,8 +197,8 @@ def cartpole(num_episodes, epsilon_decay, verbose = True, make_plots = True):
             break
         if verbose and episode % 100 == 0:
             print('[Episode {}] - Mean survival time over last 100 episodes was {}.'.format(episode, mean_score))
-            
-
+        
+        #experience replay - update Q approximation
         dqn_solver.experience_replay()
         
         if verbose:
@@ -192,4 +213,10 @@ def cartpole(num_episodes, epsilon_decay, verbose = True, make_plots = True):
     return score_list, mean_score_list, num_trials
             
 if __name__ == "__main__":
-    cartpole(1000, 0.9995)
+    
+    #log directory for TensorBoard
+    log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    #to activate Tensorboard:
+    #tensorboard --logdir logs
+    
+    cartpole(1000, 0.9995, log_dir)
