71c569056bb6c747f91b2228e352c43f23e1f437
Dan Arnold <dbarnold@lbl.gov>
2020-04-30 22:41:39 -0700

    added A2C acrobot
    A2C acrobot and small change to PG_cartpole

--- Actor Critic/A2C_acrobot.py
+++ Actor Critic/A2C_acrobot.py
@@ -18,8 +18,8 @@ import tensorflow.keras.losses as kls
 import tensorflow.keras.optimizers as ko
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--batch_size', type=int, default=64)
-parser.add_argument('-n', '--num_updates', type=int, default=250)
+parser.add_argument('-b', '--batch_size', type=int, default=128)
+parser.add_argument('-e', '--max_epochs', type=int, default=300)
 parser.add_argument('-lr', '--learning_rate', type=float, default=7e-3)
 parser.add_argument('-r', '--render_test', action='store_true', default=False)
 parser.add_argument('-p', '--plot_results', action='store_true', default=False)
@@ -36,8 +36,12 @@ class ProbDistribution(tf.keras.Model):
 class Model(tf.keras.Model):
     def __init__(self, action_space):
         super().__init__('mlp_policy')
+        # we'll use one network with multiple outputs for actor and critic
+        #hidden layers for AC
         self.hidden1 = kl.Dense(128, activation='relu')
+        self.hidden2 = kl.Dense(128, activation='relu')
         #output layers
+        self.value = kl.Dense(1, name="value")
         self.logits = kl.Dense(action_space, name='policy_logits')
         self.dist = ProbDistribution()
         
@@ -45,21 +49,22 @@ class Model(tf.keras.Model):
         #inputs are numpy array, that we convert to a tensor
         x = tf.convert_to_tensor(inputs)
         hidden_logits = self.hidden1(x)
-        return self.logits(hidden_logits)
+        hidden_value = self.hidden2(x)
+        return self.logits(hidden_logits), self.value(hidden_value)
     
-    def get_action(self, obs):
+    def get_action_value(self, obs):
         #compute logits and value
-        logits = self.predict_on_batch(obs)
+        logits, value = self.predict_on_batch(obs)
         #compute actions
         action = self.dist.predict_on_batch(logits)
-        return np.squeeze(action, axis=-1)
+        return np.squeeze(action, axis=-1), np.squeeze(value, axis=-1)
 
 #####################################
 ############## Agent ################
 #####################################  
 
 class A2CAgent:
-    def __init__(self, model, lr=7e-3, gamma=0.99, value_c=0.5, entropy_c=1e-4):
+    def __init__(self, model, lr=7e-3, gamma=0.99, value_c=0.5, entropy_c=1e-1):
         self.lr = lr
         self.gamma = gamma
         self.value_c = value_c
@@ -68,25 +73,27 @@ class A2CAgent:
         self.model = model
         self.model.compile(
             optimizer=ko.Adam(lr=self.lr),
-            loss=self._logits_loss)
+            loss=[self._logits_loss, self._value_loss])
 
 
-    def train(self, env, batch_sz=64, updates=350):
+    def train(self, env, batch_sz, max_epochs):
         #need to account for the fact that the batch size may 
         #cross episodes
         
         #storage helpers for single batch of data
         actions = np.empty((batch_sz,), dtype=np.int32)
-        rewards, dones = np.empty((2, batch_sz))
+        rewards, dones, values = np.empty((3, batch_sz))
         observations = np.empty((batch_sz,)  + env.observation_space.shape)
         #training loop
         ep_rewards = [0.0]
         next_obs = env.reset()
-        for update in range(updates):
+        epoch = 0
+        #for update in range(updates):
+        while(epoch < max_epochs):
             for step in range(batch_sz):
                 observations[step] = next_obs.copy()
                 #get the action and value for this state
-                actions[step] = self.model.get_action(next_obs[None, :])
+                actions[step], values[step] = self.model.get_action_value(next_obs[None, :])
                 #advance the environment
                 next_obs, rewards[step], dones[step], _ = env.step(actions[step])
                 
@@ -95,38 +102,44 @@ class A2CAgent:
                     ep_rewards.append(0.0)
                     next_obs = env.reset()
                     logging.info("Episode: %03d, Reward: %03d" % (len(ep_rewards)-1, ep_rewards[-2]))
+                    epoch += 1
             
             #if episode isn't done, will need V(s_tp1) for advantage
-            returns, advs = self._returns_advantages(rewards, dones)
+            _, next_value = self.model.get_action_value(next_obs[None, :])
+            returns, advs = self._returns_advantages(rewards, dones, values, next_value)
             #trick to input actions and advantages into same API
             acts_and_advs = np.concatenate([actions[:, None], advs[:, None]], axis=-1)
+            #acts_and_advs = np.concatenate([actions[None, :], advs[None, :]], axis=-1)
             #train with the batch
-            #losses = self.model.train_on_batch(observations, [acts_and_advs, returns])
-            losses = self.model.train_on_batch(observations, acts_and_advs)
-            logging.debug("[%d/%d] Losses: %s" % (update + 1, updates, losses))
+            losses = self.model.train_on_batch(observations, [acts_and_advs, returns])
+            logging.debug("[%d/%d] Losses: %s" % (epoch + 1, max_epochs, losses))
             
         return ep_rewards
  
     def test(self, env, render=False):
         state, done, episode_reward = env.reset(), False, 0
         while not done:
-            action = self.model.get_action(state[None, :])
+            action, _ = self.model.get_action_value(state[None, :])
             state, reward, done, _ = env.step(action)
             episode_reward += reward
             if render:
                 env.render()
         return episode_reward
     
-    def _returns_advantages(self, rewards, dones):
-        returns = rewards.copy()
+    def _returns_advantages(self, rewards, dones, values, next_value):
+        returns = np.append(np.zeros_like(rewards), next_value, axis=-1)
+        #Rt = rt + gamma * V(s_t+1)
         #super clever way to do this...
-        for t in reversed(range(rewards.shape[0] - 1)):
+        for t in reversed(range(rewards.shape[0])):
             returns[t] = rewards[t] + self.gamma * returns[t+1] * (1 - dones[t])
-        #compute advantages = returns - baseline
-        baseline = np.mean(returns) * np.ones_like(returns)
-        disc_returns = (returns - baseline)
-        advantages = disc_returns / np.std(disc_returns)
-        return disc_returns, advantages
+        returns = returns[:-1]
+        #compute advantages = returns - V
+        advantages = returns - values
+        return returns, advantages
+    
+    def _value_loss(self, returns, values):
+        #minimize mse between estimates and returns
+        return self.value_c * kls.mean_squared_error(returns, values)
     
     def _logits_loss(self, actions_and_advantages, logits):
         actions, advantages = tf.split(actions_and_advantages, 2, axis=-1)
@@ -152,18 +165,21 @@ if __name__ == "__main__":
     args = parser.parse_args()
     logging.getLogger().setLevel(logging.INFO)
     
-    env = gym.make('CartPole-v0')
+    env = gym.make('Acrobot-v1')
     model = Model(action_space=env.action_space.n)
     agent = A2CAgent(model, args.learning_rate)
     
     print("Training model...")
-    rewards_history = agent.train(env, args.batch_size, args.num_updates)
+    rewards_history = agent.train(env, args.batch_size, args.max_epochs)
     print("Training complete.  Testing...")
-    print("Total Episode Reward: %d out of 200" % agent.test(env, args.render_test))
+    print("Total Episode Reward: %d" % agent.test(env, args.render_test))
     
     if args.plot_results:
         plt.style.use('seaborn')
-        plt.plot(np.arange(0, len(rewards_history), 10), rewards_history[::10])
+        plt.plot(np.arange(0, len(rewards_history)), rewards_history, label='rewards')
+        plt.plot(np.arange(0, len(rewards_history), 10), rewards_history[::10], label='average_rewards(10)')
         plt.xlabel('Episode')
         plt.ylabel('Total Reward')
+        plt.title('Rewards in Training')
+        plt.legend()
         plt.show()
